fa89dc6508bb1ba6d67f94df7bba552a9eb82f51
Gildardo Ortiz <gildardo.ortiz@seguritech.com>
2023-01-18 18:58:16 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    //var socket = new SockJS('http://localhost:8094/notifier/ws/general');
+	var socket = new SockJS('https://sdc-residencial-dev.seguritech.org/api/notifier/ws/general');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+       // stompClient.subscribe('/topic/sdc-general/refresh', function (greeting) {
+		   stompClient.subscribe('/topic/chat/private.076f6098-81f6-4cf3-915a-7ee2e8eab640', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    //$( "#send" ).click(function() { sendName(); });
+});
